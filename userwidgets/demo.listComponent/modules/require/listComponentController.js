b055d51b830e0483dc2022cbcdb489cac266cb33
Kenny Sierra <kennysierra@Kennys-MacBook-Pro.local>
2019-09-25 13:18:21 -0600

    animate row

--- userwidgets/demo.listComponent/modules/require/listComponentController.js
+++ userwidgets/demo.listComponent/modules/require/listComponentController.js
@@ -9,7 +9,31 @@ define(function() {
 
 		},
       catchInfo: function(widgetinfo, data){
-		alert('Click on row ' + data.row);
+        var transformPropEnd = kony.ui.makeAffineTransform();
+		transformPropEnd.translate(-220, 0);
+        var animationDef = {
+          "100": {
+			transform: transformPropEnd,
+			stepConfig: {
+				timingFunction: kony.anim.LINEAR
+				}
+			}
+        };
+        var animationConfig = {
+          rows: [{ sectionIndex: data.section, rowIndex: data.row }],
+          widgets: ['profileContainer'],
+          animation: {
+            definition: kony.ui.createAnimation(animationDef),
+            config: {
+              delay: 0,
+              iterationCount: 1,
+              fillMode: kony.anim.FILL_MODE_FORWARDS,
+			  duration: 0.3,
+			  direction: kony.anim.DIRECTION_ALTERNATE
+            }
+          }
+        };
+		this.view.segmentList.animateRows(animationConfig);
       }
 	};
 });
