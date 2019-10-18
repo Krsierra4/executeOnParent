37bfa126e51ce7ae96dd243db6943812c5086f8f
Kenny Sierra <kennysierra@Kennys-MacBook-Pro.local>
2019-10-18 10:51:12 -0600

    update

--- userwidgets/demo.listComponent/modules/require/listComponentController.js
+++ userwidgets/demo.listComponent/modules/require/listComponentController.js
@@ -1,39 +1,39 @@
-define(function() {
+define(function () {
 
-	return {
-		constructor: function(baseConfig, layoutConfig, pspConfig) {
+  return {
+    constructor: function (baseConfig, layoutConfig, pspConfig) {
 
-		},
-		//Logic for getters/setters of custom properties
-		initGettersSetters: function() {
+    },
+    //Logic for getters/setters of custom properties
+    initGettersSetters: function () {
 
-		},
-      catchInfo: function(widgetinfo, data){
-        var transformPropEnd = kony.ui.makeAffineTransform();
-		transformPropEnd.translate(-220, 0);
-        var animationDef = {
-          "100": {
-			transform: transformPropEnd,
-			stepConfig: {
-				timingFunction: kony.anim.LINEAR
-				}
-			}
-        };
-        var animationConfig = {
-          rows: [{ sectionIndex: data.section, rowIndex: data.row }],
-          widgets: ['profileContainer'],
-          animation: {
-            definition: kony.ui.createAnimation(animationDef),
-            config: {
-              delay: 0,
-              iterationCount: 1,
-              fillMode: kony.anim.FILL_MODE_FORWARDS,
-			  duration: 0.3,
-			  direction: kony.anim.DIRECTION_ALTERNATE
-            }
+    },
+    catchInfo: function (widgetinfo, data) {
+      var transformPropEnd = kony.ui.makeAffineTransform();
+      transformPropEnd.translate(-220, 0);
+      var animationDef = {
+        "100": {
+          transform: transformPropEnd,
+          stepConfig: {
+            timingFunction: kony.anim.LINEAR
           }
-        };
-		this.view.segmentList.animateRows(animationConfig);
-      }
-	};
+        }
+      };
+      var animationConfig = {
+        rows: [{ sectionIndex: data.section, rowIndex: data.row }],
+        widgets: ['profileContainer'],
+        animation: {
+          definition: kony.ui.createAnimation(animationDef),
+          config: {
+            delay: 0,
+            iterationCount: 1,
+            fillMode: kony.anim.FILL_MODE_FORWARDS,
+            duration: 0.3,
+            direction: kony.anim.DIRECTION_ALTERNATE
+          }
+        }
+      };
+      this.view.segmentList.animateRows(animationConfig);
+    }
+  };
 });
