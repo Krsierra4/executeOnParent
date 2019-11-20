395fc798dbdb72ec37320557eef120810823b515
Kenny Sierra <kennysierra@Kennys-MacBook-Pro.local>
2019-11-20 14:34:15 -0600

    delete the olc function

--- controllers/tablet/profileContainerController/profileContainerController.js
+++ controllers/tablet/profileContainerController/profileContainerController.js
@@ -1,12 +1,5 @@
 define({ 
 
-  onClickImage: function(context){
-    var info = {
-      row: context.rowIndex,
-      section: context.sectionIndex
-    };
-    this.executeOnParent('catchInfo', info);
-  },
   onClickAction: function(eventobject, context) {
     var secIndex = context.sectionIndex;
     var rowIndex = context.rowIndex;
