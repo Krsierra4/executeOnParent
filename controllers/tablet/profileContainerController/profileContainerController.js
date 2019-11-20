e3f2c79af4a7e5866e626c516c05e9c821d00360
Kenny Sierra <kennysierra@Kennys-MacBook-Pro.local>
2019-11-20 14:33:14 -0600

    change actions

--- controllers/tablet/profileContainerController/profileContainerController.js
+++ controllers/tablet/profileContainerController/profileContainerController.js
@@ -6,5 +6,14 @@ define({
       section: context.sectionIndex
     };
     this.executeOnParent('catchInfo', info);
+  },
+  onClickAction: function(eventobject, context) {
+    var secIndex = context.sectionIndex;
+    var rowIndex = context.rowIndex;
+    var rowInfo = {
+      section : secIndex,
+      row : rowIndex
+    };
+    this.executeOnParent('onClickAction', rowInfo);
   }
  });
--- controllers/tablet/profileContainerController/profileContainerControllerActions.js
+++ controllers/tablet/profileContainerController/profileContainerControllerActions.js
@@ -2,9 +2,9 @@ define({
     /*
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
-    /** onClick defined for FlexContainer0a8954225fb6a4a **/
-    AS_FlexContainer_e46b0200366c4f1e8847cb3c5e458e42: function AS_FlexContainer_e46b0200366c4f1e8847cb3c5e458e42(eventobject, context) {
+    /** onClick defined for flxAction **/
+    AS_FlexContainer_ab2d8be5edc04ad3a0de2e374e3306a8: function AS_FlexContainer_ab2d8be5edc04ad3a0de2e374e3306a8(eventobject, context) {
         var self = this;
-        return self.onClickImage.call(this, context);
+        return self.onClickAction.call(this, eventobject, context);
     }
 });
